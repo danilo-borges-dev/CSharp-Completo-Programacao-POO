0617bf4ab36a62ed3f4abdb588a925bd6efe6f44
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-08-25 08:35:38 -0300

    Exercicio resolvido com sucesso

---
+++ PrimeiroProgramaCSharpDoCurso/Exercicios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Console.WriteLine("\n");
+
+string Produto1 = "Computador";
+string Produto2 = "Mesa de Escritório";
+
+byte Idade = 30;
+int Codigo = 5290;
+char Genero = 'M';
+
+double Preco1 = 2100.0;
+double Preco2 = 650.50;
+double Medida = 53.234567;
+
+Console.WriteLine("Produtos: ");
+Console.WriteLine($"{Produto1}, cujo o preço é ${Preco1:F2}");
+Console.WriteLine($"{Produto2}, cujo o preço é ${Preco2:F2}\n\n");
+
+Console.WriteLine($"Registro: {Idade} anos de idade, código {Codigo} e gênero: {Genero}\n\n");
+
+
+Console.WriteLine($"Medida com oito casa decimais: {Medida:F8}");
+Console.WriteLine($"Arredondado (três casa decimais): {Medida:F3}");
+Console.WriteLine($"Separador decimal invariant culture: {Medida.ToString("F3", CultureInfo.InvariantCulture)}");
+
---
+++ PrimeiroProgramaCSharpDoCurso/SaidaDeDados/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+string Nome = "Danilo";
+int Idade = 29;
+char Sexo = 'M';
+double Salario = 5_320.55789;
+
+// Controlar a Quantidade de Casas Decimais que serão exibidas:
+Console.WriteLine(Salario.ToString("F2")); // Duas casas Decimais
+Console.WriteLine(Salario.ToString("C"));  // Resultado no console R$ 5.320,55
+
+Console.WriteLine("\n");
+
+// Apresentar o Resultado com o Ponto como separador das Casas Decimais:
+Console.WriteLine(Salario.ToString("F2", CultureInfo.InvariantCulture));  // Resultado no Console 5320.55 - FormatProvider, como eu gostaria de formatar o meu número
+
+Console.WriteLine("\n");
+
+// Placeholders:
+Console.WriteLine("{0} tem {1} anos de idade e o seu Salário é {2:F2}.\n", Nome, Idade, Salario); // Pode alterar {2:C}
+
+// Concatenação:
+Console.WriteLine(Nome + " tem " + Idade + " anos de idade e o seu Salário é " + Salario.ToString("F2", CultureInfo.InvariantCulture) + ".\n");
+
+// Interpolação:    
+Console.WriteLine($"{Nome} tem {Idade} anos de idade e o seu Salário é {Salario:F2}.");  // Pode alterar {2:C}
+
