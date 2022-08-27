06698a414e7dd485809f4bfa123f04c7cc1364a0
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-08-27 09:22:23 -0300

    Pratincando com Classes

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+//string[] s = Console.ReadLine().Split(" ");
+
+//string a = s[0];
+//string b = s[1];
+//string c = s[2];
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+
+Console.WriteLine($"Digite um valor: ");
+double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine(n.ToString("F2", CultureInfo.InvariantCulture));
---
+++ PrimeiroProgramaCSharpDoCurso/PraticandoComVetor/Program.cs
@@ -0,0 +1,13 @@
+﻿//string[] s = Console.ReadLine().Split(" ");
+
+//string a = s[0];
+//string b = s[1];
+//string c = s[2];
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+//Console.WriteLine(c);
+
+int numero1 = int.Parse(Console.ReadLine());
+double numero2 = double.Parse(Console.ReadLine());
+char c = char.Parse(Console.ReadLine());
