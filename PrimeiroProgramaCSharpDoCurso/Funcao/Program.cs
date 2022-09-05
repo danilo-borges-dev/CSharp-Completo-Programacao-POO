3efc7fe044342c0da256ecd2cbf495370a139b97
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-05 06:54:17 -0300

    Trabalhando com funcao

---
+++ PrimeiroProgramaCSharpDoCurso/Funcao/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite três números: ");
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+
+int maior = new int();
+
+if (n1 > n2 && n1 > n3)
+{
+    maior = n1;
+}
+else if (n2 > n3)
+{
+    maior = n2;
+}
+else
+{
+    maior = n3;
+}
+
+Console.WriteLine($"Mair número digitado: {maior}");
