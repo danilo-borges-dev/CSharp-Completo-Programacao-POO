bc27c0c135aabe5816662d8ab8d69f5b5192088c
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-12 07:25:11 -0300

    Treinando a criacao de Classes

---
+++ PrimeiroProgramaCSharpDoCurso/Triangulo_03/Program.cs
@@ -0,0 +1,18 @@
+﻿using Triangulo_03;
+using System.Globalization;
+
+Triangulo x = new();
+Triangulo y = new();
+
+Console.WriteLine("Entre com as medidas do triângulo X: ");
+x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Entre com as medidas do triângulo Y: ");
+y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Área de X = " + x.CalcularArea().ToString("F4", CultureInfo.InvariantCulture));
+Console.WriteLine("Área de Y = " + y.CalcularArea().ToString("F4", CultureInfo.InvariantCulture));
---
+++ PrimeiroProgramaCSharpDoCurso/Triangulo_03/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿namespace Triangulo_03
+{
+    internal class Triangulo
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double CalcularArea()
+        {
+            double p = (A + B + C) / 2.0;
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
+    }
+}
