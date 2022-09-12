a1dd53a9b5443c8e9ab22a38cf2429f547780d00
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-12 08:44:24 -0300

    Exercicio 02 resulvido com sucesso

---
+++ PrimeiroProgramaCSharpDoCurso/ProdutosEmEstoque/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace ProdutosEmEstoque
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QtdEstoque { get; set; }
+
+        public void AdicionarNoEstoque(int qtd)
+        {
+            QtdEstoque += qtd;
+            ToString();
+        }
+
+        public void RemoverDoEstoque(int qtd)
+        {
+            QtdEstoque -= qtd;
+            ToString();
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"\nDados atualizados: {Nome}, ${Preco.ToString("F2", CultureInfo.InvariantCulture)}, {QtdEstoque} unidades, Total: ${(Preco * QtdEstoque).ToString("F2", CultureInfo.InvariantCulture)}");
+            return "";
+        }
+    }
+}
