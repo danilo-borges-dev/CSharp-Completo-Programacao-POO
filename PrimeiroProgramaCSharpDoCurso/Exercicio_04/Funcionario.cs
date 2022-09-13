07024ea36c0e9c00d6ffa32fc1b0e8e3495f4070
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-13 08:33:25 -0300

    Exercicio 04 finalizado com sucesso

---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_04/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exercicio_04
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double SalarioLiquido { get; set; }
+        public double Imposto { get; set; }
+
+        public void DescontarImposto()
+        {
+            SalarioLiquido = SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioLiquido += (porcentagem / 100) * SalarioBruto;
+            ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Dados atualizados: {Nome}, ${SalarioLiquido.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_04/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercicio_04;
+using System.Globalization;
+
+Funcionario f = new();
+
+Console.Write("Nome: ");
+f.Nome = Console.ReadLine();
+
+Console.Write("Salário bruto: ");
+f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Imposto: ");
+f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+f.DescontarImposto();
+
+Console.WriteLine($"\nFuncíonário {f.Nome}, ${f.SalarioLiquido.ToString("F2", CultureInfo.InvariantCulture)}");
+
+Console.Write($"\nDigite a porcentagem para aumentar o salário: ");
+double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+f.AumentarSalario(porcentagem);
+
+
+Console.WriteLine($"\n{f}");
