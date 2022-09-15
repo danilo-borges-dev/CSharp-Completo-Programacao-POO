0fa2b3a7681c5011680b05b17243047a3bffa18f
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 08:00:17 -0300

    Trabalhando com classe estatica de uma forma um pouco diferente no programa principal

--- PrimeiroProgramaCSharpDoCurso/ClasseEstatica2/Program.cs
+++ PrimeiroProgramaCSharpDoCurso/ClasseEstatica2/Program.cs
@@ -4,6 +4,8 @@ double raio = new double();
 double volume = new double();
 double circunferencia = new double();
 
+Main();
+
 void Main()
 {
     Console.Write("Entre com o valor do raio: ");
@@ -12,6 +14,7 @@ void Main()
     circunferencia = Circunferencia.CalcularCircunferencia(raio);
     volume = Volume.CalcularVolume(raio);
 
+    // Exibição de String via Console com PlaceHolders
     Console.WriteLine("\nCircunferência: {0}", circunferencia.ToString("F2", CultureInfo.InvariantCulture));
     Console.WriteLine("Volume: {0}", volume.ToString("F2", CultureInfo.InvariantCulture));
     Console.WriteLine("PI: {0}", Math.PI.ToString("F2", CultureInfo.InvariantCulture));
@@ -29,7 +32,7 @@ public static class Volume
 {
     public static double CalcularVolume(double raio)
     {
-        return 4.0 / 3.0 * Math.PI * raio;
+        return 4.0 / 3.0 * Math.PI * Math.Pow(raio, 3);
     }
 }
 
