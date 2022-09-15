d2f0bed6818e16ec3c0432d935a72af82ef458ba
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 08:22:01 -0300

    Trabalhando com classe estatica de uma forma um pouco diferente no programa principal

---
+++ PrimeiroProgramaCSharpDoCurso/ClasseEstatica2/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿namespace ClasseEstatica2
+{
+    internal class Calculadora
+    {
+        public double PI = 3.14;
+        public double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        public double Volume(double raio)
+        {
+            return 4.0 / 3.0 * PI * raio * raio * raio;
+        }
+    }
+}
--- PrimeiroProgramaCSharpDoCurso/ClasseEstatica2/Program.cs
+++ PrimeiroProgramaCSharpDoCurso/ClasseEstatica2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using ClasseEstatica2;
+using System.Globalization;
 
 double raio = new double();
 double volume = new double();
@@ -11,29 +12,34 @@ void Main()
     Console.Write("Entre com o valor do raio: ");
     raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-    circunferencia = Circunferencia.CalcularCircunferencia(raio);
-    volume = Volume.CalcularVolume(raio);
+    Calculadora c = new();
+
+    circunferencia = c.Circunferencia(raio);
+    volume = c.Volume(raio);
 
     // Exibição de String via Console com PlaceHolders
     Console.WriteLine("\nCircunferência: {0}", circunferencia.ToString("F2", CultureInfo.InvariantCulture));
     Console.WriteLine("Volume: {0}", volume.ToString("F2", CultureInfo.InvariantCulture));
     Console.WriteLine("PI: {0}", Math.PI.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
 }
 
-public static class Circunferencia
-{
-    public static double CalcularCircunferencia(double raio)
-    {
-        return 2.0 * Math.PI * raio;
-    }
-}
+//public static class Circunferencia
+//{
+//    public static double CalcularCircunferencia(double raio)
+//    {
+//        return 2.0 * Math.PI * raio;
+//    }
+//}
 
-public static class Volume
-{
-    public static double CalcularVolume(double raio)
-    {
-        return 4.0 / 3.0 * Math.PI * Math.Pow(raio, 3);
-    }
-}
+//public static class Volume
+//{
+//    public static double CalcularVolume(double raio)
+//    {
+//        return 4.0 / 3.0 * Math.PI * Math.Pow(raio, 3);
+//    }
+//}
 
 
