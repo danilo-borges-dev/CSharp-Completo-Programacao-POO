8e5da28c9349c63550f1896c966ca52c77404a1a
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 07:10:58 -0300

    Praticando com exercicios

---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_06/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Digite em uma única linha três cores: ");
+string[] s = Console.ReadLine().Split(" ");
+
+string a = s[0];
+string b = s[1];
+string c = s[2];
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
