5dd7e9852590275fc120fc899213267203b76b01
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-15 08:44:13 -0300

    Trabalhando com classe estatica de uma forma um pouco diferente no programa principal

---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_07/ConversorDeMoeda.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercicio_07
+{
+
+    internal class ConversorDeMoeda
+    {
+        public static double ConverterMoeda(double cotacao, double quantidade)
+        {
+            double valorASerpago = new double();
+            valorASerpago = (cotacao * quantidade);
+            valorASerpago += (cotacao * quantidade) * 0.06;
+            //return (cotacao * quantidade) * 0.06;
+            return valorASerpago;
+        }
+    }
+}
---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_07/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercicio_07;
+using System.Globalization;
+
+double cotacaoDolar = new double();
+double quantidadeQueIraComprar = new double();
+double valorASerPago = new double();
+
+Console.Write("Qual é a cotação do dólar? ");
+cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dólares você vai comprar? ");
+quantidadeQueIraComprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+valorASerPago = ConversorDeMoeda.ConverterMoeda(cotacaoDolar, quantidadeQueIraComprar);
+
+Console.WriteLine($"Valor a ser pago em reais: {valorASerPago.ToString("F2", CultureInfo.InvariantCulture)}");
