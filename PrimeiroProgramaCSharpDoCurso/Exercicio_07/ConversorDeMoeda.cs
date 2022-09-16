6ccfedfaf05d24f8e25fb90e6da7cd0c734f682d
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-16 07:14:11 -0300

    Trabalhando com classe estatica de uma forma um pouco diferente no programa principal

--- PrimeiroProgramaCSharpDoCurso/Exercicio_07/ConversorDeMoeda.cs
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_07/ConversorDeMoeda.cs
@@ -6,9 +6,8 @@
         public static double ConverterMoeda(double cotacao, double quantidade)
         {
             double valorASerpago = new double();
-            valorASerpago = (cotacao * quantidade);
+            valorASerpago = cotacao * quantidade;
             valorASerpago += (cotacao * quantidade) * 0.06;
-            //return (cotacao * quantidade) * 0.06;
             return valorASerpago;
         }
     }
---
+++ PrimeiroProgramaCSharpDoCurso/Exercicio_08/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+string nome;
+string[] sTexto;
+int nQuatros = new int();
+double pPreco = new double();
+
+Console.WriteLine("Entre com o seu nome completo: ");
+nome = Console.ReadLine();
+Console.WriteLine("Quantos quartos tem na sua casa?");
+nQuatros = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre com o preço de um produto: ");
+pPreco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+sTexto = Console.ReadLine().Split(" ");
+
+Console.WriteLine($"\n{nome}");
+Console.WriteLine($"{nQuatros}");
+Console.WriteLine($"{pPreco.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"{sTexto[0]}");
+Console.WriteLine($"{sTexto[1]}");
+Console.WriteLine($"{sTexto[2]}");
