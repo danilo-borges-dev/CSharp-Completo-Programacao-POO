a7288d950aad7cc4b195a5cfe90915ea0128f52d
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-16 07:32:49 -0300

    Entendendo melhor e trabalhando com onstrutures de classe

---
+++ PrimeiroProgramaCSharpDoCurso/Construtores/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace Construtores
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public string Codigo { get; set; }
+        public Produto() { }
+
+        public Produto (string nome, double valor, string codigo = "100")
+        {
+            Nome = nome;
+            Valor = valor;
+            Codigo = codigo;
+        }
+    }
+}
