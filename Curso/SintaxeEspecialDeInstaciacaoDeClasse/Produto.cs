c1b4cf245c6aaae801199c0bc2c88b1c29d9c04b
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-19 08:58:16 -0300

    Realizando novos exercicios

---
+++ Curso/SintaxeEspecialDeInstaciacaoDeClasse/Produto.cs
@@ -0,0 +1,15 @@
+﻿namespace SintaxeEspecialDeInstaciacaoDeClasse
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+    }
+}
---
+++ Curso/SintaxeEspecialDeInstaciacaoDeClasse/Program.cs
@@ -0,0 +1,9 @@
+﻿using SintaxeEspecialDeInstaciacaoDeClasse;
+
+string nome = "Batata";
+double valor = 5_00;
+int quantidade = 100;
+
+Produto p = new(nome, valor) { Quantidade = quantidade };
+
+Console.WriteLine("Hello, World!");
