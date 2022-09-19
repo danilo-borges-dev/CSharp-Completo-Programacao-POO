c9583497a54f7ab491e32a9b0041c5cd3058a0aa
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-19 08:01:06 -0300

    Realizando novos exercicios

---
+++ Curso/Exercicio_12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+Main();
+
+void Main()
+{
+    Console.Write("Qual é a cotação do dólar? ");
+    double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    Console.Write("Quantos dólares você vai comprar? ");
+    double qtdDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    double valorAPagarEmReais = ConversorDeMoeda.CalcularCotacao(cotacao, qtdDolares);
+
+    Console.WriteLine($"Valor a ser pago em reais = {valorAPagarEmReais.ToString("F2", CultureInfo.InvariantCulture)}");
+
+}
+
+public static class ConversorDeMoeda
+{
+    public static double CalcularCotacao(double cotacao, double quantidade)
+    {
+        double valorAPagarEmReais = cotacao * quantidade; 
+        valorAPagarEmReais += valorAPagarEmReais * 0.06;
+        return valorAPagarEmReais;
+    }
+}
