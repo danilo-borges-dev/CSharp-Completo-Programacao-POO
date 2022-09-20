e4127fbe11e67bb33c11894d6317f4e8d1d3a56e
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-20 08:14:08 -0300

    Trabalhando com Encapsulamento

--- Curso/Encapsulamento/Produto.cs
+++ Curso/Encapsulamento/Produto.cs
@@ -6,12 +6,25 @@
         private int Quantidade { get; set;  }
         private double Valor { get; set; }
 
+        public Produto(string nome, int quantidade, double valor)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Valor = valor;
+        }
+
         public void DefinirNome(string nome)
         {
             if (String.IsNullOrEmpty(nome))
             {
                 Nome = nome;
             }
+            throw new ArgumentNullException(message: "Preencha o Campo Nome");
         }
-    }
+
+        public string GetNomeDeCliente()
+        {
+            return Nome;
+        }
+    } 
 }
--- Curso/Encapsulamento/Program.cs
+++ Curso/Encapsulamento/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Encapsulamento;
+
+Produto p = new("Feijão", 10, 5.50);
+
+try
+{
+    p.DefinirNome(" ");
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Está tentando definir um nome vazio ou que possua números.", ex.Message.ToString());
+}
+
+Console.WriteLine(p.GetNomeDeCliente());
