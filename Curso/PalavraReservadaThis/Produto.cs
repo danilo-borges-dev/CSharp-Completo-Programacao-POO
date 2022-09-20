97d16c2c557f2aa7ff7d46db3eb505d3fac480c4
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-20 07:05:31 -0300

    Palavra this

---
+++ Curso/PalavraReservadaThis/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace PalavraReservadaThis
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Valor { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto() { }
+        public Produto(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+        public Produto(string nome, double valor, int quantidade) : this (nome, valor)
+        {
+            Quantidade = quantidade;
+        }
+    }
+}
