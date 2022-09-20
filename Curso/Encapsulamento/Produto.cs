224783127efb459a2034cd691390497475b86586
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-20 07:55:37 -0300

    Trabalhando com Encapsulamento

---
+++ Curso/Encapsulamento/Produto.cs
@@ -0,0 +1,17 @@
+﻿namespace Encapsulamento
+{
+    internal class Produto
+    {
+        private string Nome { get; set; }
+        private int Quantidade { get; set;  }
+        private double Valor { get; set; }
+
+        public void DefinirNome(string nome)
+        {
+            if (String.IsNullOrEmpty(nome))
+            {
+                Nome = nome;
+            }
+        }
+    }
+}
