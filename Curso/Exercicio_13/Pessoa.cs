103fd4d55c5e556869e4c58a90f8f691a521c70c
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-21 07:21:14 -0300

    Finzalido um exercicio

---
+++ Curso/Exercicio_13/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio_13
+{
+    internal class Pessoa
+    {
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+
+        public Pessoa (string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+
+        public int Getidade()
+        {
+            return Idade;
+        }
+    }
+}
---
+++ Curso/Exercicio_13/Program.cs
@@ -0,0 +1,24 @@
+﻿using Exercicio_13;
+
+List<Pessoa> pessoaList = new();
+
+for (int i = 0; i <= 1; i++)
+{
+    Console.WriteLine($"Dados da {i + 1}º pessoa: ");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Idade: ");
+    int idade = int.Parse(Console.ReadLine());
+
+    pessoaList.Add(new Pessoa(nome, idade));
+}
+
+
+if (pessoaList[0].Getidade() > pessoaList[1].Getidade())
+{
+    Console.Write($"Pessoa mais velha: {pessoaList[0].GetNome()}");
+}
+else
+{
+    Console.Write($"Pessoa mais velha: {pessoaList[1].GetNome()}");
+}
