a26d0020301965ac53e89a5bd105d268e3ad1bbc
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-21 07:05:38 -0300

    Revisando construtores

---
+++ Curso/Construtores/Produto_02.cs
@@ -0,0 +1,26 @@
+﻿namespace Construtores
+{
+    internal class Produto_02
+    {
+        private string Nome { get; set; }
+        private int _quantidade = new int();
+        private double _peso;
+        public double Valor { get; private set; }
+
+        public Produto_02(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+
+        public Produto_02(string nome, double valor, int quantidade ) : this (nome, valor)
+        {
+            _quantidade = quantidade;
+        }
+
+        public Produto_02(string nome, double valor, int quantidade, double peso) : this (nome, valor, quantidade)
+        {
+            _peso = peso;
+        }
+    }
+}
