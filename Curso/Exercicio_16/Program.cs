32c521e4fc55fff5a064155528060138a478a59a
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-26 07:06:44 -0300

    Resolvendo exercicios

---
+++ Curso/Exercicio_16/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercicio_16;
+using System.Globalization;
+
+Console.WriteLine("Entre com a Largura e Altura de um Retângulo:");
+double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Retangulo r = new(altura, largura);
+
+Console.Write($"\nÁrea = {r.CalcularArea().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.Write($"\nPerímetro = {r.CalcularPerimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.Write($"\nDiagonal = {r.CalcularDiagonal().ToString("F2", CultureInfo.InvariantCulture)}");
---
+++ Curso/Exercicio_16/Retangulo.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercicio_16
+{
+    internal class Retangulo
+    {
+        public double Altura { get; private set; }
+        public double Largura { get; private set; }
+
+        public Retangulo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double CalcularArea()
+        {
+            return Largura * Altura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * Largura + 2 * Altura;
+        } 
+
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2) + (Math.Pow(Altura, 2)));
+        }
+    }
+}
