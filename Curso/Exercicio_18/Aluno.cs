eed1f6dd968b61ab8b3506a0c37f450b78ba82d9
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-26 07:33:03 -0300

    Resolvendo exercicios

---
+++ Curso/Exercicio_18/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Exercicio_18
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+        public double[] Notas { get; private set; } = new double[3];
+
+        public Aluno(string nome, double[] notas)
+        {
+            Nome = nome;
+            Notas = notas;
+        }
+
+        private double NotaFinal()
+        {
+            return Notas[0] + Notas[1] + Notas[2];
+        }
+        public void VerificarAprovacao()
+        {
+            if (NotaFinal() < 60)
+            {
+                Console.WriteLine($"Nota Final = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Reprovado");
+                Console.WriteLine($"Faltam {(60 - NotaFinal()).ToString("F2", CultureInfo.InvariantCulture)} pontos");
+            }
+            else
+            {
+                Console.WriteLine($"Nota Final = {NotaFinal().ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Aprovado");
+            }
+        }
+    }
+}
---
+++ Curso/Exercicio_18/Program.cs
@@ -0,0 +1,15 @@
+﻿using Exercicio_18;
+using System.Globalization;
+
+double[] notas = new double[3];
+
+Console.Write("Nome do aluno: ");
+string nome = Console.ReadLine();
+Console.WriteLine("Digite as três notas do aluno:");
+notas[0] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+notas[1] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+notas[2] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Aluno a = new(nome, notas);
+
+a.VerificarAprovacao();
