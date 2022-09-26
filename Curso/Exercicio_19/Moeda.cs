549d892abd57062c433ba1435dabc98877f7d1ae
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-26 07:39:37 -0300

    Resolvendo exercicios

---
+++ Curso/Exercicio_19/Moeda.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio_19
+{
+    internal static class Moeda
+    {
+        public static double CalcularCotacao(double cotacaoDolar, double qtdDolar)
+        {
+            return ((cotacaoDolar * qtdDolar) * 0.06) + (cotacaoDolar * qtdDolar);
+        }
+    }
+}
---
+++ Curso/Exercicio_19/Program.cs
@@ -0,0 +1,8 @@
+﻿using Exercicio_19;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dolar? ");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantos dolares você vai comprar? ");
+double qtdDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write($"Valor a ser pago em reais: {Moeda.CalcularCotacao(cotacao, qtdDolares).ToString("F2", CultureInfo.InvariantCulture)}");
