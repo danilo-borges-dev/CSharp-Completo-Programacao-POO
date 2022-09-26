ff0a1712a20f3e3d5b149c0fceee25f3449aa0e1
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-26 07:20:56 -0300

    Resolvendo exercicios

---
+++ Curso/Exercicio_17/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Exercicio_17
+{
+    internal class Funcionario
+    {
+        public string Nome { get; private set; }
+        public double SalarioBruto { get; private set; }
+        public double Imposto { get; private set; }
+        public double NovoSalario { get; private set; }
+
+        public Funcionario(string nome, double salarioBruto, double imposto)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+        }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double procentagem)
+        {
+            NovoSalario = (SalarioBruto + (SalarioBruto * (procentagem / 100))) - Imposto;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados atualizados: {Nome}, $ {NovoSalario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Curso/Exercicio_17/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercicio_17;
+using System.Globalization;
+
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+Console.Write("Salário bruto: ");
+double salarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Imposto: ");
+double imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Funcionario f = new(nome, salarioBruto, imposto);
+
+Console.WriteLine($"Funcionário: {f.Nome}, $ {f.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
+Console.Write("\nDigite a porcetangem para aumentar o salário: ");
+double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+f.AumentarSalario(porcentagem);
+
+Console.WriteLine(f);
