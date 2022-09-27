c785224ac52ae286edec8a83c18b7fc23522b796
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-27 08:30:41 -0300

    Trabalhando com Structs e tipo referencia

---
+++ Curso/ConsoleApp3/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp3
+{
+    internal struct Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+}
---
+++ Curso/ConsoleApp3/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp3;
+
+Point p = new();
+p.X = 12;
+p.Y = 5;
+
+Console.WriteLine(p);
+
+
+Point px = new();
+Console.WriteLine(px);
