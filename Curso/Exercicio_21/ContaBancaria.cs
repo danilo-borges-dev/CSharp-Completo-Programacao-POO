af11fdb85f287f78172580ccb1bec9961e6e6271
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-27 07:23:18 -0300

    Finalizado mais um exercicio

---
+++ Curso/Exercicio_21/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Exercicio_21
+{
+    internal class ContaBancaria
+    {
+        public readonly int NumeroConta;
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Saldo = 0.0;
+        }
+
+        public ContaBancaria(int numeroConta, string titular, double saldo) : this(numeroConta, titular)
+        {
+            Deposito(saldo);
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + 5.0);
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados da conta atualizados:" +
+                $"\nConta {NumeroConta}, Titular {Titular}, Saldo $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Curso/Exercicio_21/Program.cs
@@ -0,0 +1,41 @@
+﻿using Exercicio_21;
+using System.Globalization;
+
+string titular;
+int numeroConta = new int();
+char opcao = new char();
+double valor = new double();
+
+ContaBancaria conta;
+
+Console.Write("Entre o número da conta: ");
+numeroConta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Entre o nome do titular: ");
+titular = Console.ReadLine();
+Console.Write("Haverá depósito inicial (s/n)? ");
+opcao = char.Parse(Console.ReadLine());
+
+if (opcao != 'n' && opcao != 'N')
+{
+    Console.Write("Entre o valor de saldo inicial: ");
+    valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    conta = new(numeroConta, titular, valor);
+}
+else
+{
+    conta = new(numeroConta, titular);
+}
+
+Console.WriteLine("\nDados da conta:");
+Console.WriteLine($"Conta {conta.NumeroConta}, Titular: {conta.Titular}, Saldo $ {conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+
+Console.Write("\nEntre com o valor para depósito: ");
+valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Deposito(valor);
+Console.WriteLine(conta);
+
+
+Console.Write("\nEntre com o valor para saque: ");
+valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+conta.Saque(valor);
+Console.WriteLine(conta);
