bf3a50fed6845df707fcfe94d3b6c89016446e9c
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-09-29 08:45:38 -0300

    Trabalhando com Nullable

--- Curso/ValorNulo/Program.cs
+++ Curso/ValorNulo/Program.cs
@@ -1,7 +1,32 @@
 ﻿
-int? x = null;
-Nullable<int> y = null;
+////int? x = null;
+////Nullable<int> y = null;
 
-string result = x == null ? "é nulo" : "não é nulo";
+////string result = x == null ? "é nulo" : "não é nulo";
 
-Console.WriteLine(result);
+////Console.WriteLine(result);
+
+//double? x = null;
+//double? y = 10.0;
+
+//Console.WriteLine(x.GetValueOrDefault());
+//Console.WriteLine(y.GetValueOrDefault());
+
+//Console.WriteLine();
+
+//Console.WriteLine(x.HasValue);
+//Console.WriteLine(y.HasValue);
+
+//Console.WriteLine();
+
+////Console.WriteLine(x.Value);
+////Console.WriteLine(y.Value);
+
+//string result = x.HasValue ? x.Value : "É nulo";
+//Console.WriteLine(result);
+
+double? x = null;
+
+double y = x ?? x.GetValueOrDefault();
+
+Console.WriteLine(y);
