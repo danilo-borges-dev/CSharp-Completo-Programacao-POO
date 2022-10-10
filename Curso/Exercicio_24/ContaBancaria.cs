151a14563c15bc75845f016bb9b3f48ce5fd693b
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-10 08:33:58 -0300

    Exercicio 23 finalizado com sucesso

---
+++ Curso/Exercicio_24/ContaBancaria.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Exercicio_24
+{
+    internal class ContaBancaria
+    {
+        public readonly string _nomeTitular;
+        public int _numeroConta { get; private set; } = new int();
+        public double _saldo { get; private set; } = new double();
+
+        public ContaBancaria(string nomeTitular, int numeroConta) 
+        {
+            _nomeTitular = nomeTitular;
+            _numeroConta = numeroConta;
+        }
+
+        public ContaBancaria(string nomeTitular, int numeroConta, double saldoInicial) : this (nomeTitular, numeroConta)
+        {
+            _saldo = saldoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                _saldo += valor;
+                Console.WriteLine("Operação de Depósito realizado com Sucesso.");
+            }
+            Console.WriteLine("Operação não Permitida, tente novamente.");
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= _saldo)
+            {
+                _saldo -= (valor + 5.0);
+                Console.WriteLine("Operação de Saque realizado com Sucesso.");
+            }
+            Console.WriteLine($"Operação não permitida. Valor para Saque $ {_saldo.ToString("F", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Curso/Exercicio_24/Program.cs
@@ -0,0 +1,30 @@
+﻿using Exercicio_24;
+using System.Globalization;
+
+int numeroConta = new int();
+string nomeTitular;
+char opcao = new char();
+double saldoInicial = new double();
+
+ContaBancaria conta;
+
+Console.Write("Entre com o número da conta: ");
+numeroConta = int.Parse(Console.ReadLine());
+Console.Write("Entre com o nome do Titular da conta: ");
+nomeTitular = Console.ReadLine();
+Console.Write("Haverá depósito inicial? (s/n) ");
+opcao = char.Parse(Console.ReadLine());
+
+if (opcao == 's' || opcao == 'S')
+{
+    Console.Write("Entre com o valor do Saldo inicial $ ");
+    saldoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    conta = new(nomeTitular, numeroConta, saldoInicial);
+}
+else
+{
+    conta = new(nomeTitular, numeroConta);
+}
+
+Console.WriteLine("\nDados da conta: ");
+Console.WriteLine($"Conta {conta._numeroConta} Titular {conta._nomeTitular}, Saldo $ {conta._saldo.ToString("F2", CultureInfo.InvariantCulture)}");
