6ac2a15be5176e6efbb4663493fcb5111161baab
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-10 08:13:31 -0300

    Exercicio 23 finalizado com sucesso

---
+++ Curso/Exercicio_23/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+string nameComplete;
+int roomsTotal = new int();
+double productPrice = new double();
+string[] s = new string[3];
+
+Console.WriteLine("Informe o seu nome completo: ");
+nameComplete = Console.ReadLine();
+Console.WriteLine("Quantos quartos tem na sua casa? ");
+roomsTotal = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre com o preço de um produto: ");
+productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Entre com o seu último nome, idade e altura: (Mesma linha)");
+s = Console.ReadLine().Split(" ");
+
+Console.WriteLine($"\n{nameComplete}");
+Console.WriteLine(roomsTotal);
+Console.WriteLine(productPrice.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine(s[0]);
+Console.WriteLine(s[1]);
+Console.WriteLine(s[2]);
