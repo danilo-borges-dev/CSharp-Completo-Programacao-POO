0c1d2e16cb8cbf93ba64921f82ddbad0d7d17fc8
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-17 07:36:44 -0300

    Finalizado o exercicio 26

---
+++ Curso/Exercicio_26/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+string[] nomes = new string[2];
+double[] salarios = new double[2];
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Dados do {i + 1}ª funcionário: ");
+    Console.Write("Nome: ");
+    nomes[i] = Console.ReadLine();
+    Console.Write("Salário $ ");
+    salarios[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+}
+
+Console.WriteLine($"Salário médio $ {((salarios[0] + salarios[1]) / 2).ToString("F2", CultureInfo.InvariantCulture)}");
