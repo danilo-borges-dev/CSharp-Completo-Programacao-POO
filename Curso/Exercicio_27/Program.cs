3476ca2b94f6294f960f0ef6f3599d1cddbc2985
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-18 07:44:52 -0300

    Finalizado o exercicio 27

--- Curso/Exercicio_27/Program.cs
+++ Curso/Exercicio_27/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_27;
+using System.Globalization;
+
+double largura = new double();
+double altura = new double();
+Retangulo r;
+
+Console.WriteLine("Entre com a largura e altura do retâmgulo: ");
+largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+r = new(largura, altura);
+Console.WriteLine($"AREA = {r.CalcularArea().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"PERÍMETRO = {r.CalcularPerimetro().ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"DIAGONAL = {r.CalcularDiagonal().ToString("F2", CultureInfo.InvariantCulture)}");
---
+++ Curso/Exercicio_27/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio_27
+{
+    internal class Retangulo
+    {
+        public double Altura { get; private set; }
+        public double Largura { get; private set; }
+        public Retangulo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double CalcularArea()
+        {
+            return Altura * Largura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+        public double CalcularDiagonal()
+        {
+            return Math.Sqrt( Math.Pow(Altura, 2) + Math.Pow(Largura, 2) );
+        }
+    }
+}
