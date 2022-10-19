14097422d5fd9987c058d31d6bf01994ba0ee015
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 06:30:43 -0300

    Exercicio 28 finalizado

---
+++ Curso/Exercicio_28/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicio_28
+{
+    internal class Funcionario
+    {
+        public string Nome { get; private set; }
+        public double SalarioBruto { get; private set; }
+        public double Imposto { get; private set; }
+
+        public Funcionario(string nome, double salarioBruto, double imposto)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+        }
+        public double CalcularSalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (porcentagem / 100));
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados atualizados: {Nome}, $ {CalcularSalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Curso/Exercicio_28/Program.cs
+++ Curso/Exercicio_28/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_28;
+using System.Globalization;
+
+Funcionario f;
+string nome;
+double salario = new double();
+double imposto = new double();
+double porcentagem = new double();
+
+Console.Write("Nome: ");
+nome = Console.ReadLine();
+Console.Write("Salario Bruto: $ ");
+salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Imposto: $ ");
+imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+f = new(nome, salario, imposto);
+
+Console.WriteLine($"\nFuncionário: {f.Nome}, {f.CalcularSalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
+Console.Write("Digite a porcentagem para aumentar o salario: ");
+porcentagem = double.Parse(Console.ReadLine());
+f.AumentarSalario(porcentagem);
+
+Console.WriteLine(f);
