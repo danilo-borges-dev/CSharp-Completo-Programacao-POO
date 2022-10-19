90722f38993bf9cb3565cffca59a4875bc2cddcd
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 06:54:39 -0300

    Finalizado o exercicio 30

---
+++ Curso/Exercicio_30/ConversorDeMoeda.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio_30
+{
+    internal class ConversorDeMoeda
+    {
+        public double RealizarConversao(double cotacaoDolar, double totalDolares)
+        {
+            return cotacaoDolar * totalDolares + (cotacaoDolar * totalDolares * 0.06);
+        }
+    }
+}
---
+++ Curso/Exercicio_30/Program.cs
@@ -0,0 +1,15 @@
+﻿using Exercicio_30;
+using System.Globalization;
+
+ConversorDeMoeda conversor;
+double cotacaoDolar = new double();
+double totalDolares = new double();
+
+Console.Write("Qual é a cotação do dolar? ");
+cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantos dolares você vai comprar? ");
+totalDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+conversor = new();
+
+Console.Write($"Valor a ser pago em reais = $ {conversor.RealizarConversao(cotacaoDolar, totalDolares).ToString("F2", CultureInfo.InvariantCulture)}");
