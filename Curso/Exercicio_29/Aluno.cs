77a6c50099e7129430aaf1bc1821b98cf7ce8209
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 06:42:26 -0300

    Exercicio 29 finalizado

---
+++ Curso/Exercicio_29/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+
+namespace Exercicio_29
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+        public double[] Notas { get; private set; } = new double[3];
+
+        public Aluno(string nome, double[] notas)
+        {
+            Nome = nome;
+            Notas = notas;
+        }
+
+        public void CalcularNotaFinal()
+        {
+            double mediaFinal = new double();
+            for (int i = 0; i < Notas.Length; i++)
+            {
+                mediaFinal += Notas[i];
+            }
+            if (mediaFinal < 60)
+            {
+                Console.WriteLine($"Nota Final {mediaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Reprovado");
+                Console.WriteLine($"Faltam {(60 - mediaFinal).ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine($"Nota Final {mediaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Aprovado");
+            }
+        }
+    }
+}
---
+++ Curso/Exercicio_29/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercicio_29;
+
+Aluno aluno;
+string nome;
+double[] notas = new double[3];
+    
+Console.Write("Informe o nome do aluno: ");
+nome = Console.ReadLine();
+Console.WriteLine("Digite as três notas do aluno: ");
+notas[0] = double.Parse(Console.ReadLine());
+notas[1] = double.Parse(Console.ReadLine());
+notas[2] = double.Parse(Console.ReadLine());
+
+aluno = new(nome, notas);
+aluno.CalcularNotaFinal();
+
