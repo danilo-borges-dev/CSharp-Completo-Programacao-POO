8c9267bb15dd2fab9df154a5fd06229c58d7dedc
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-19 07:01:53 -0300

    Finalizado o exercicio 30

---
+++ Curso/RevisaoMetodoDeExtensao/Program.cs
@@ -0,0 +1,6 @@
+﻿using RevisaoMetodoDeExtensao;
+
+string mensagem = "Olá Mundo!";
+Console.WriteLine(mensagem);
+string mensagemInvertida = mensagem.Reverter();
+Console.WriteLine(mensagemInvertida);
---
+++ Curso/RevisaoMetodoDeExtensao/ReverterStringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace RevisaoMetodoDeExtensao
+{
+    internal static class ReverterStringExtensions
+    {
+        public static string Reverter(this string valor)
+        {
+            return new string(valor.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
