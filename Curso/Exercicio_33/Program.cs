03e7839212d727a646b48b84a25dc2ad74a049a5
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-25 06:31:41 -0300

    Finalizado exercicio 33

---
+++ Curso/Exercicio_33/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] nome = new string[2];
+int[] idade = new int[2];
+
+for (int i = 0; i < 2; i++)
+{
+    Console.WriteLine($"Dados da {i + 1}ª pessoa:");
+    Console.Write("Nome: ");
+    nome[i] = Console.ReadLine();
+    Console.Write("Idade: ");
+    idade[i] = int.Parse(Console.ReadLine());
+}
+
+if (idade[0] > idade[1])
+{
+    Console.WriteLine($"\nPessoa mais velha: {nome[0]}");
+}
+else
+{
+    Console.WriteLine($"\nPessoa mais velha: {nome[1]}");
+}
