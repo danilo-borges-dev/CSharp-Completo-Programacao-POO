8b876ac3efcd9b5a8ad8a918f7ca31fca2ee0eb4
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-26 07:07:23 -0300

    Exercicio 38 Finalizado

---
+++ Curso/Exercicio_38/ConversorDeMoeda.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio_38
+{
+    internal static class ConversorDeMoeda
+    {
+        public static double Converter(double valorDolar, double quantidade)
+        {
+            return (valorDolar * quantidade) + (valorDolar * quantidade) * 0.06;
+        }
+    }
+}
---
+++ Curso/Exercicio_38/Program.cs
@@ -0,0 +1,12 @@
+﻿using Exercicio_38;
+using System.Globalization;
+
+double valorDolar = new double();
+double qtdDolares = new double();
+
+Console.Write("Valor do Dolar: $ ");
+valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantos Dolares irá Comprar? ");
+qtdDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"\nValor a pagar pelos Dolares: $ {(ConversorDeMoeda.Converter(valorDolar, qtdDolares)).ToString("F2", CultureInfo.InvariantCulture)}");
