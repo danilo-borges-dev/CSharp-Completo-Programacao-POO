47f4eed78dedfe1880bbf5db2f1c6fb14e53c3c2
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-26 06:59:48 -0300

    Exercicio 37 finalizado

---
+++ Curso/Exercicio_37/Aluno.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace Exercicio_37
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+        public double[] Notas { get; private set; } = new double[3];
+        private string _situacaoAluno;
+        public Aluno(string nome, double[] notas)
+        {
+            Nome = nome;
+            Notas = notas;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double notaFinal = new double();
+            for (int i = 0; i < Notas.Length; i++)
+            {
+                notaFinal += Notas[i];
+            }
+            return notaFinal;
+        }
+
+        public override string ToString()
+        {
+            double notaFinal = CalcularNotaFinal();
+            if (CalcularNotaFinal() < 60)
+            {
+                _situacaoAluno = "Reprovado";
+                return $"\nNota Final {notaFinal.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                    $"{_situacaoAluno}\n" +
+                    $"Faltam: {(60 - notaFinal).ToString("F2", CultureInfo.InvariantCulture)} pontos";
+            }
+            _situacaoAluno = "Aprovado";
+            return $"\nNota Final {CalcularNotaFinal().ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"{_situacaoAluno}";
+        }
+    }
+}
---
+++ Curso/Exercicio_37/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercicio_37;
+using System.Globalization;
+
+string nome;
+double[] notas = new double[3];
+Aluno aln;
+
+Console.WriteLine("Digite o nome do aluno: ");
+nome = Console.ReadLine();
+Console.WriteLine("Digite as três notas do aluno: ");
+notas[0] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+notas[1] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+notas[2] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+aln = new(nome, notas);
+
+aln.CalcularNotaFinal();
+Console.WriteLine(aln);
+
