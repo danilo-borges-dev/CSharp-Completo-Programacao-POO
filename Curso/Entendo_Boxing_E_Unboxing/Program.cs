2b0834eaee7bab20cee4d3c7d33a7d1a32caab4a
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-10-29 15:26:57 -0300

    ref e out

---
+++ Curso/Entendo_Boxing_E_Unboxing/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// Este valor está sendo Salvo na memória Stack
+int valorStack = 10;
+
+// A variável valorHead está sendo Salva na Memória Stack apontando para
+// o Valor está sendo Salvo na Memória Head - Aqui temos um Boxing
+object valorHead = 10;
+
+int valor = (int)valorHead;
