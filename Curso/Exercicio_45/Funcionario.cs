6e01733f89af58352157cc6e4a08921fa63584c0
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-04 07:20:20 -0300

    Revisando Arrays

---
+++ Curso/Exercicio_45/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_45
+{
+    internal class Funcionario
+    {
+        public readonly int Id = new int();
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario += (Salario * porcentagem / 100);
+        }
+    }
+}
---
+++ Curso/Exercicio_45/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Exercicio_45;
+
+int nFuncionarios = new int();
+int idFuncionario = new int();
+double salario = new double();
+double porcentagem = new double();
+bool condicao = true;
+string nomeFuncionario;
+
+List<Funcionario> listaDeFuncionarios = new();
+
+Console.Write("Informe o Número de Funcionários: ");
+nFuncionarios = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < nFuncionarios; i++)
+{
+    do
+    {
+        Console.Write($"\nInforme o ID do {i + 1}º Funcionáriom: ");
+        idFuncionario = int.Parse(Console.ReadLine());
+
+        foreach (var item in listaDeFuncionarios)
+        {
+            if (item.Id == idFuncionario)
+            {
+                Console.WriteLine($"Já Existe Funcionário com Está ID {item.Id}. Tente outra ID.");
+                condicao = true;
+            }
+            else
+            {
+                condicao = false;
+            }
+        }
+
+    } while (condicao);
+    Console.Write("Nome: ");
+    nomeFuncionario = Console.ReadLine();
+    Console.WriteLine("Salário $ ");
+    salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    listaDeFuncionarios.Add(new Funcionario(idFuncionario, nomeFuncionario, salario));
+}
+
+Console.WriteLine("\n");
+
+foreach (var item in listaDeFuncionarios)
+{
+    Console.WriteLine($"ID {item.Id}# Nome: {item.Nome}, Salário $ {item.Salario.ToString("F2", CultureInfo.InvariantCulture)}");
+}
+
+do
+{
+    Console.Write("\nInforme o ID de um Funcionário para Aumentar o Salário: ");
+    idFuncionario = int.Parse(Console.ReadLine());
+
+    foreach (var item in listaDeFuncionarios)
+    {
+        if (item.Id == idFuncionario)
+        {
+            Console.Write("Informe a porcentagem para Aumentar o Salário: ");
+            porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            item.AumentarSalario(porcentagem);
+            condicao = false;
+        }
+    }
+
+    if (condicao == false)
+    {
+        Console.Write($"ID {idFuncionario} não Localizado, tente novamente!");
+        condicao = true;
+    }
+
+} while (condicao);
