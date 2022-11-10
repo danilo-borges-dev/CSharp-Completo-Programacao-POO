de4b192363b845b6464414ee4bff30129fb46014
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-10 06:29:24 -0300

    Revisando Arrays

---
+++ Curso/Revisando_Arrays_2/Models/Atletas.cs
@@ -0,0 +1,29 @@
+﻿namespace Revisando_Arrays_2.Models
+{
+    internal class Atletas : IComparable<Atletas>
+    {
+        public int Id { get; private set; } = new int();
+        public double Peso { get; private set; } = new double();
+        public string Nome { get; private set; }
+
+        public Atletas(int id, double peso, string nome)
+        {
+            Id = id;
+            Peso = peso;
+            Nome = nome;
+        }
+
+        public int CompareTo(Atletas? other)
+        {
+            if (this.Id == other.Id)
+            {
+                return 0;
+            }
+            if (this.Id > other.Id)
+            {
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Curso/Revisando_Arrays_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using Revisando_Arrays_2.Models;
+
+int[] i = new int[2]
+{
+    2, 3
+};
+
+Atletas[] listaAtletas = new Atletas[5];
+
+listaAtletas[0] = new Atletas(10, 50.0, "João Paulo");
+listaAtletas[1] = new Atletas(1, 50.0, "Marcos Silva");
+listaAtletas[2] = new Atletas(5, 50.0, "Rosana Pereira");
+listaAtletas[3] = new Atletas(8, 50.0, "Natália Garcia");
+listaAtletas[4] = new Atletas(9, 50.0, "Roberto Caramelo");
+
+Array.Sort(listaAtletas);
+
+foreach (var item in listaAtletas)
+{
+    Console.WriteLine(item.Id);
+    Console.WriteLine(item.Nome);
+}
