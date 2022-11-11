8df5e851552d906842bd4d22f9977c60b4b1b370
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-11 07:06:56 -0300

    Entendo Composicao na Pratica

--- Curso/Exercicio_52/Models/Worker.cs
+++ Curso/Exercicio_52/Models/Worker.cs
@@ -1,4 +1,5 @@
 ﻿using Exercicio_52.Models.Enums;
+using Exercicio_52.Models;
 
 namespace Exercicio_52.Models
 {
@@ -8,12 +9,14 @@ namespace Exercicio_52.Models
         public WorkerLevel Level { get; private set; } = new WorkerLevel();
         public double BaseSalary { get; private set; } = new double();
         public Department Department { get; set; }  // Nesta Linha temos uma composição de Objetos
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
 
-        public Worker(string name, WorkerLevel level, double baseSalary)
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
         {
             Name = name;
             Level = level;
             BaseSalary = baseSalary;
+            Department = department;
         }
 
         public void AddContract(HourContract contract)
