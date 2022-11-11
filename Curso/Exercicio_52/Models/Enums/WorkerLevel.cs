cc2fefe787a883a985626390c476922feabaf631
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-11 06:51:59 -0300

    Entendo Composicao

---
+++ Curso/Exercicio_52/Models/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio_52.Models.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        Md_Level = 1,
+        Senior = 2,
+    }
+}
---
+++ Curso/Exercicio_52/Models/Worker.cs
@@ -0,0 +1,33 @@
+﻿using Exercicio_52.Models.Enums;
+
+namespace Exercicio_52.Models
+{
+    internal class Worker
+    {
+        public string Name { get; private set; }
+        public WorkerLevel Level { get; private set; } = new WorkerLevel();
+        public double BaseSalary { get; private set; } = new double();
+
+        public Worker(string name, WorkerLevel level, double baseSalary)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+
+        }
+
+        public void RemoveContract(HourContract contract)
+        {
+
+        }
+
+        public double inCome(int year, int month)
+        {
+
+        }
+    }
+}
