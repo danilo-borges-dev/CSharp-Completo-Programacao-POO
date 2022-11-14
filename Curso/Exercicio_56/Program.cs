191cad028b2e224b7d5ead67713f644d617f9d11
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-14 06:54:20 -0300

    Finalizado o Exercicio 55

---
+++ Curso/Exercicio_56/Program.cs
@@ -0,0 +1,50 @@
+﻿int m = new int();
+int n = new int();
+
+int[,] mat;
+
+Console.WriteLine("Informe o valor de M: ");
+m = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o valor de N: ");
+n = int.Parse(Console.ReadLine());
+
+mat = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    string[] s = Console.ReadLine().Split(" ");
+
+    for (int j = 0; j < n; j++)
+    {
+        mat[i, j] = int.Parse(s[n]);
+    }
+}
+
+Console.Write("\nDentre os valores inteiros digitados, informe um: ");
+m = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (j > 0)
+        {
+            Console.WriteLine("Lefht: " + mat[i, j - 1]);
+        }
+
+        if (i > 0)
+        {
+            Console.WriteLine("Down: " + mat[i - 1, j]);
+        }
+
+        if (mat[i, j - 1] > 0)
+        {
+            Console.WriteLine("Rigth: " + mat[i,j + 1]);
+        }
+
+        if (mat[i - 1, j] > 0)
+        {
+            Console.WriteLine("Top: " + mat[i + 1, j]);
+        }
+    }
+}
---
+++ Curso/Exercicio_57/Program.cs
@@ -0,0 +1,38 @@
+﻿
+int n = new int();
+int negativos = new int();
+int[,] mat;
+
+Console.Write("Informa um número inteiro: ");
+n = int.Parse(Console.ReadLine());
+
+mat = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] s = Console.ReadLine().Split(" ");
+
+    for (int j = 0; j < n; j++)
+    {
+        mat[i,j] = int.Parse(s[j]);
+
+        if (mat[i,j] < 0)
+        {
+            negativos++;
+        }
+    }
+}
+
+Console.WriteLine("\nNegatives numbers = " + negativos);
+Console.Write("Main diagonal: ");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j)
+        {
+            Console.Write(mat[i,j] + " ");
+        }
+    }
+}
