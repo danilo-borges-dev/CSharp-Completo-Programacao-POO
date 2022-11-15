9d62a992dea293fee4092d440eb5042278bebe28
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-15 07:25:59 -0300

    Revendo Heranca

---
+++ Curso/Ententando_Heranca/Models/Account.cs
@@ -0,0 +1,29 @@
+﻿namespace Ententando_Heranca.Models
+{
+    internal class Account
+    {
+        public int Number { get; protected set; } = new int();
+        public double Balance { get; protected set; } = new double();
+        public string Holder { get; protected set; }
+
+        public Account(int number, double balance, string holder)
+        {
+            Number = number;
+            Balance = balance;
+            Holder = holder;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ Curso/Ententando_Heranca/Models/BusinesAccount.cs
@@ -0,0 +1,20 @@
+﻿namespace Ententando_Heranca.Models
+{
+    internal class BusinesAccount : Account
+    {
+        public double LoanLimit { get; private set; } = new double();
+
+        public BusinesAccount(int number, double balance, string holder, double loanLimit) : base(number, balance, holder)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
