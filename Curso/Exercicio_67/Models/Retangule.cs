70073d839e97bcdb0de8744dbe53be39ae0b0fad
Danilo Borges <daniloborges@corp.gera>
2022-11-17 13:59:16 -0300

    Entendendo metodos abstratos

---
+++ Curso/Exercicio_67/Models/Retangule.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercicio_67.Models
+{
+    internal sealed class Retangule : Shape
+    {
+        public double Width { get; private set; } = new double();
+        public double Heigth { get; private set; } = new double();
+
+        public Retangule(string color, double width, double heigth) : base (color)
+        {
+            Width = width;
+            Heigth = heigth;
+        }
+
+        public override double Area()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Curso/Exercicio_67/Models/Shape.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio_67.Models
+{
+    internal abstract class Shape
+    {
+        public string Color { get; protected set; }
+
+        public Shape(string color)
+        {
+            Color = color;
+        }
+        public abstract double Area();
+    }
+}
