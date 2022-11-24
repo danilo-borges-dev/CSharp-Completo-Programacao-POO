d4ac7d4df6aeee5326359231ad62984c792b5bac
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-24 06:40:25 -0300

    Finalizado o exercicio 79

---
+++ Curso/Exercicio_79/Models/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercicio_79.Models
+{
+    internal class Funcionario
+    {
+        public int Id { get; private set; } = new int();
+        public string Nome { get; private set; }
+        public double Salario { get; private set; } = new double();
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+    }
+}
---
+++ Curso/Exercicio_79/Program.cs
@@ -0,0 +1,56 @@
+﻿using Exercicio_79.Models;
+using System.Globalization;
+
+int qtdFuncionarios = new int();
+int id = new int();
+string nomeFuncionario;
+double salarioFuncionario = new double();
+double porcentagemAumentoSalario = new double();
+bool condicao = true;
+
+List<Funcionario> listaDeFuncionarios = new List<Funcionario>();
+
+Console.Write("Quantos funcionários existem na empresa? ");
+qtdFuncionarios = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < qtdFuncionarios; i++)
+{
+    Console.WriteLine($"\nFuncionário #{i + 1}: ");
+    Console.Write("ID: ");
+    id = int.Parse(Console.ReadLine());
+    Console.Write("Nome: ");
+    nomeFuncionario = Console.ReadLine();
+    Console.Write("Salário $ ");
+    salarioFuncionario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    listaDeFuncionarios.Add(new Funcionario(id, nomeFuncionario, salarioFuncionario));
+}
+
+Console.Write("\nInforme o ID do Funcionário que terá aumento de Salário: ");
+id = int.Parse(Console.ReadLine());
+
+while (condicao)
+{
+    foreach (Funcionario funcionario in listaDeFuncionarios)
+    {
+        if (id == funcionario.Id)
+        {
+            Console.Write("Informe o valor da Porcentagem de Aumento: ");
+            porcentagemAumentoSalario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(porcentagemAumentoSalario);
+            condicao = false;
+        }
+    }
+
+    if (condicao = true)
+    {
+        Console.Write($"\nID {id} de Funcionário não Existe.\nDigite outro: ");
+        id = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nLista de Funcionários: ");
+foreach (Funcionario funcionario in listaDeFuncionarios)
+{
+    Console.WriteLine($"{funcionario.Id}, {funcionario.Nome}, {funcionario.Salario.ToString("F2", CultureInfo.InvariantCulture)}");
+}
