aac9b484df9050dade6fc042003c251c75034c96
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-28 08:43:57 -0300

    Finalizado o Exercicio 82

---
+++ Curso/Exercicio_82/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int integerNumber = new int();
+int[,] mat;
+int negativeNumbersTotal = new int();
+
+Console.Write("Informe um número inteiro: ");
+integerNumber = int.Parse(Console.ReadLine());
+mat = new int[integerNumber, integerNumber];
+
+for (int i = 0; i < integerNumber; i++)
+{
+    Console.Write($"\nInforme na mesma linha {integerNumber} números = ");
+    string[] numbers = Console.ReadLine().Split(" ");
+
+    for (int j = 0; j < integerNumber; j++)
+    {
+        mat[i, j] = int.Parse(numbers[j]);
+        if (mat[i, j] < 0)
+        {
+            negativeNumbersTotal++;
+        }
+    }
+}
+
+Console.WriteLine();
+for (int i = 0; i < integerNumber; i++)
+{
+    for (int j = 0; j < integerNumber; j++)
+    {
+        if (i == j)
+        {
+            Console.Write($"{mat[i,j]}, ");
+        }
+    }
+}
+
+Console.WriteLine($"\nNumbers negatives total = {negativeNumbersTotal}");
