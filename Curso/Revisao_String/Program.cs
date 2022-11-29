be301565d80c43f3c9b8bba5d2479bba9a01cf4e
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-11-29 05:48:37 -0300

    Revisando os metodos de Manipulacao de Strings

---
+++ Curso/Revisao_String/Program.cs
@@ -0,0 +1,10 @@
+﻿string msg = "  Que a mente seja plena para construir e mentalizar coisas boas na vida no presente e no futuro  ";
+
+Console.WriteLine(msg.ToUpper());
+Console.WriteLine(msg.ToLower());
+Console.WriteLine(msg.Trim());
+Console.WriteLine(msg.IndexOf("para"));
+Console.WriteLine(msg.Substring(10, 10));
+Console.WriteLine(msg.Replace("para", "PARA"));
+Console.WriteLine(String.IsNullOrEmpty(msg));
+Console.WriteLine(String.IsNullOrWhiteSpace(msg));
