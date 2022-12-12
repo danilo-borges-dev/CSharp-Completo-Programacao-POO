72beb10c629833ca9433fe45666e6ca6b4b25e18
Danilo da Silva Borges <danilosilva2017@hotmail.com>
2022-12-12 06:11:42 -0300

    Exercicio 92 finalizado

---
+++ Curso/Exercicio_92/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int[,] mat;
+int n = new int();
+int qtdNegativos = new int();
+
+Console.Write("Informe o valor de n: ");
+n = int.Parse(Console.ReadLine());
+
+mat = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"\nInforme {n} números na mesma linha: ");
+    string[] s = Console.ReadLine().Split(" ");
+    for (int j = 0; j < n; j++)
+    {
+        mat[i, j] = int.Parse(s[j]);
+        if (mat[i,j] < 0)
+        {
+            qtdNegativos++;
+        }
+    }
+}
+
+Console.WriteLine("\nDiagonal: ");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j)
+        {
+            Console.Write(mat[i, j] + " ");
+        }
+    }
+}
+Console.WriteLine("Total de negativos: " + qtdNegativos);
