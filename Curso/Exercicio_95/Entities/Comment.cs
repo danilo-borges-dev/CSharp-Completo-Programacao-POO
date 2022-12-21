bbaa6e4785780898825c35dd8116fa0eefadcd3f
danilo borges <danilosilva2017@hotmail.com>
2022-12-21 07:49:08 -0300

    Exercicio 94 resolvido com sucesso

---
+++ Curso/Exercicio_95/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio_95.Entities
+{
+    internal class Comment
+    {
+        public string Text { get; private set; }
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Curso/Exercicio_95/Entities/Post.cs
@@ -0,0 +1,7 @@
+﻿namespace Exercicio_95.Entities
+{
+    internal class Post
+    {
+        public DateTime MyProperty { get; set; }
+    }
+}
