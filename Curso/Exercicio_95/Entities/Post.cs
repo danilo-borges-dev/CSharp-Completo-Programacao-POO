504fb2448dc3afd5a44c01972add031164059686
danilo borges <danilosilva2017@hotmail.com>
2022-12-22 05:22:59 -0300

    Exercicio 95 resolvido com sucesso

--- Curso/Exercicio_95/Entities/Post.cs
+++ Curso/Exercicio_95/Entities/Post.cs
@@ -1,7 +1,43 @@
-﻿namespace Exercicio_95.Entities
+﻿using System.Text;
+
+namespace Exercicio_95.Entities
 {
     internal class Post
     {
-        public DateTime MyProperty { get; set; }
+        public DateTime Moment { get; private set; }
+        public string Title { get; private set; }
+        public string Contente { get; private set; }
+        public int Likes { get; private set; }
+
+        public List<Comment> CommentsList = new List<Comment>();
+        public Post(DateTime moment, string title, string contente, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Contente = contente;
+            Likes = likes;
+        }
+        public void AddComment(Comment comment)
+        {
+            CommentsList.Add(comment);
+        }
+        public void RemoveComment(Comment comment)
+        {
+            CommentsList.Remove(comment);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Momment: {Moment.ToString()}");
+            sb.AppendLine($"Title: {Title}");
+            sb.AppendLine($"Contet: {Contente}");
+            sb.AppendLine("Comments: ");
+            foreach (Comment comment in CommentsList)
+            {
+                sb.AppendLine(comment.Text);
+            }
+            sb.AppendLine($"Likes: {Likes.ToString()}");
+            return sb.ToString();
+        }
     }
 }
--- Curso/Exercicio_95/Program.cs
+++ Curso/Exercicio_95/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_95.Entities;
+
+Post post1 = new(new DateTime(2018, 06, 21, 13, 05, 44), "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+Post post2 = new(new DateTime(2018, 07, 28, 23, 14, 19), "Good night guys", "See you tomorrow", 5);
+
+post1.AddComment(new Comment("Have a nice trip"));
+post1.AddComment(new Comment("Wow that's awesome!"));
+
+post2.AddComment(new Comment("Good night"));
+post2.AddComment(new Comment("May the Force with you"));
+
+Console.WriteLine(post1);
+Console.WriteLine("\n" + post2);
+
