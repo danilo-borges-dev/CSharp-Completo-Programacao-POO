e98316b0ef066748537dc6b51edb413aa78046ab
danilo borges <danilosilva2017@hotmail.com>
2022-12-27 05:18:12 -0300

    Exercicio 103

---
+++ Curso/Exercicio_103/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int number = new int();
+int[,] mat;
+int negativeNumber = new int();
+
+Console.Write("Informe um número inteiro: ");
+number = int.Parse(Console.ReadLine());
+mat = new int[number, number];
+
+for (int i = 0; i < number; i++)
+{
+	Console.Write($"Digite {number} quantidade de números na mesma linha: ");
+	string[] numbers = Console.ReadLine().Split(" ");
+	for (int j = 0; j < number; j++)
+	{
+		mat[i, j] = int.Parse(numbers[j]);
+		if (mat[i,j] < 0)
+		{
+			negativeNumber++;
+        }
+	}
+}
+
+Console.WriteLine("\nMain diagonal: ");
+for (int i = 0; i < number; i++)
+{
+	for (int j = 0; j < number; j++)
+	{
+		if (i == j)
+		{
+			Console.Write($"{mat[i,j]} ");
+		}
+	}
+}
+
+Console.WriteLine("\nNegative numbers: " + negativeNumber);
