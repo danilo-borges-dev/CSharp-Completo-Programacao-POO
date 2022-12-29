486965cf92eb602f29399762aae6cc27b6865d7a
danilo borges <danilosilva2017@hotmail.com>
2022-12-29 06:37:42 -0300

    Exercicio 104 resolvido com sucesso

---
+++ Curso/Exercicio_104/Entities/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercicio_104.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; private set; }
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ Curso/Exercicio_104/Program.cs
@@ -0,0 +1,32 @@
+﻿using Exercicio_104.Entities;
+using Exercicio_104.Services;
+using System.Globalization;
+
+string model;
+DateTime start = new DateTime();
+DateTime finish = new DateTime();
+double pricePerHour = new double();
+double pricePerDay = new double();
+
+CarRental carRental;
+RentalService rentalservices;
+
+Console.WriteLine("Enter rental data: ");
+Console.Write("Car model: ");
+model = Console.ReadLine();
+Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+start = DateTime.Parse(Console.ReadLine());
+Console.Write("Return (dd/MM/yyyy hh:mm): ");
+finish = DateTime.Parse(Console.ReadLine());
+Console.Write("Enter price per hour: $ ");
+pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter price per day: $ ");
+pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+carRental = new(start, finish, new Vehicle(model));
+rentalservices = new(pricePerHour, pricePerDay);
+
+rentalservices.ProcessInvoice(carRental);
+
+Console.WriteLine("\nINVOICE");
+Console.WriteLine(carRental.Invoice);
