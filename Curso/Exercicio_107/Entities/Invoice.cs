02915a906b78afd6ccbb10954789581d870328c1
danilo borges <danilosilva2017@hotmail.com>
2023-01-02 07:39:23 -0300

    Exercicio 107 finalizado com sucesso

---
+++ Curso/Exercicio_107/Entities/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace Exercicio_107.Entities
+{
+    internal class Invoice
+    {
+        public double BasicaPayment { get; private set; }
+        public double Tax { get; private set; }
+        public Invoice(double basicaPayment, double tax)
+        {
+            BasicaPayment = basicaPayment;
+            Tax = tax;
+        }
+        public double TotalPayment()
+        {
+            return BasicaPayment + Tax;
+        }
+        public override string ToString()
+        {
+            return $"\nBasic payment $ {BasicaPayment.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"Tax $ {Tax.ToString("F2", CultureInfo.InvariantCulture)}\n" +
+                $"Total payment $ {TotalPayment().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Curso/Exercicio_107/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio_107.Entities;
+using Exercicio_107.Services;
+using System.Globalization;
+
+Console.WriteLine("Enter car data:");
+Console.Write("Car model: ");
+string carModel = Console.ReadLine();
+Console.Write("Pickup (dd/MM/yyyy): ");
+DateTime start = DateTime.Parse(Console.ReadLine());
+Console.Write("Return (dd/MM/yyyy): ");
+DateTime finish = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Enter price per hour $ ");
+double pricePerhour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter price per day $ ");
+double pricePerDay= double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+CarRental carRental = new(start, finish, new Vehicle(carModel));
+
+RentalService rentalService = new(pricePerhour, pricePerDay);
+
+rentalService.ProcessInvoice(carRental);
+
+Console.WriteLine(carRental.Invoice);
+
