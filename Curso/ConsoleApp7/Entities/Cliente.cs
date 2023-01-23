a8633bd652f797d61a936580ecf12af97bb0f8ac
danilo borges <danilosilva2017@hotmail.com>
2023-01-23 05:23:55 -0300

    Metodo bodied

---
+++ Curso/ConsoleApp7/Entities/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp7.Entities
+{
+    internal class Cliente
+    {
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public string NomeCompleto => Nome + Sobrenome;
+        public string NomeCompleto2() => Nome + Sobrenome;
+    }
+}
