4a3172a60e6f4dff96ca844d7f77c4713da5ef4a
danilo borges <danilosilva2017@hotmail.com>
2023-01-27 05:30:16 -0300

    Exercicio 124 realizado com sucesso

---
+++ Curso/Exercicio_124/Models/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio_124.Models
+{
+    internal class Funcionario
+    {
+        public int Id { get; } 
+        public string Nome { get; private set; } = string.Empty;
+        public double Salario { get; private set; } = new double();
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentarSalario (double porcentagem)
+        {
+            if (porcentagem > 0.0)
+            {
+                Salario = ((porcentagem / 100) * Salario) + Salario;
+            }
+        }
+    }
+}
---
+++ Curso/Exercicio_124/Program.cs
@@ -0,0 +1,66 @@
+﻿using Exercicio_124.Models;
+using static System.Console;
+using System.Globalization;
+
+int qtdFuncionarios = new int();
+int id = new int();
+string nome = string.Empty;
+double salario = new double();
+bool condicao = new bool();
+double percentualAumento = new double();
+
+List<Funcionario> listaFuncionarios = new List<Funcionario>();
+
+Write("Quantos funcionários? ");
+qtdFuncionarios = int.Parse(ReadLine());
+
+for (int i = 0; i < qtdFuncionarios; i++)
+{
+    WriteLine($"\nFuncionário #{i + 1}");
+    Write("ID: ");
+    id = int.Parse(ReadLine());
+    Write("Nome: ");
+    nome = ReadLine();
+    Write("Salary $ ");
+    salario = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+
+    listaFuncionarios.Add(new Funcionario(id, nome, salario));
+}
+
+condicao = true;
+
+do
+{
+    Write("\nInforme o ID do Funcionário para ajuste de salário: ");
+    id = int.Parse(ReadLine());
+
+    foreach (var funcionario in listaFuncionarios)
+    {
+        if (id == funcionario.Id)
+        {
+            Write("Informe o Percentual de aumento: ");
+            percentualAumento = double.Parse(ReadLine(), CultureInfo.InvariantCulture);
+            funcionario.AumentarSalario(percentualAumento);
+            condicao = false;
+        }
+    }
+
+    if (condicao == true)
+    {
+        WriteLine("ID de funcionário não localizado - Tente novamente:");
+    }
+    else
+    {
+        WriteLine("Aumento realizado com sucesso!");
+        WriteLine("Funcionários atualizados: ");
+        foreach (var funcionario in listaFuncionarios)
+        {
+            WriteLine($"ID {funcionario.Id} - {funcionario.Nome}, $ {funcionario.Salario.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+
+} while (condicao);
+
+WriteLine("Fim do programa");
+
+
